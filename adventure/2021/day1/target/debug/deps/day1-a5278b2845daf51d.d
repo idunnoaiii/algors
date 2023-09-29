a954dd735319abcbfcfb312d1f91ebb419c3302e
Nguyễn Quang Thiện <84729328+idunnoaiii@users.noreply.github.com>
2023-09-29 19:59:47 +0700

    Delete adventure/2021/day1/target directory
    delete

--- adventure/2021/day1/target/debug/deps/day1-a5278b2845daf51d.d
+++
@@ -1,9 +0,0 @@
-/mnt/E6241EAC241E7FAB/codes/rust/algors/adventure/2021/day1/target/debug/deps/day1-a5278b2845daf51d: src/main.rs src/part2.rs src/part2/code.rs src/part1.rs src/part1/code.rs
-
-/mnt/E6241EAC241E7FAB/codes/rust/algors/adventure/2021/day1/target/debug/deps/day1-a5278b2845daf51d.d: src/main.rs src/part2.rs src/part2/code.rs src/part1.rs src/part1/code.rs
-
-src/main.rs:
-src/part2.rs:
-src/part2/code.rs:
-src/part1.rs:
-src/part1/code.rs:
